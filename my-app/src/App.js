5cd5a04883d3d0552c39af435f8576c99322eaaf
Farel Lukas <farellukas@gmail.com>
2023-02-11 08:59:50 -0500

    Added sign up page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
-import BrandExample from "./Components/Navbar";
-import Home from "./Routes/Home";
-
-function App() {
-  return (
-    <div className="App">
-      <BrandExample />
-      <Home />
-    </div>
-=======
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from "./Components/Navbar";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import { AuthContextProvider } from './Context/AuthContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import { AuthContextProvider } from "./Context/AuthContext";
 
 function App() {
   return (
@@ -30,11 +16,8 @@ function App() {
             <Route path="register" element={<Signup />} />
           </Routes>
         </AuthContextProvider>
-
       </div>
     </BrowserRouter>
-
->>>>>>> a715969f00306bfc0bb313b03920990657ccfc5c
   );
 }
 
